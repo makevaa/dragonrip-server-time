f5602c9b08d1ab1b3775ec05d71864f1df2d2ffd
Markus <markus.vaananen@gmail.com>
2025-04-22 00:26:26 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Dragonrip Server Time 
+
+🐵 Tampermonkey userscript to show a server time clock for [the browser game Dragonrip](https://dragonrip.com/).
+The time element is inserted at the bottom of the game logo element.
+
+🍴 Available on GreasyFork: [greasyfork.org/en/scripts/532949-dragonrip-toolbar](https://greasyfork.org/en/scripts/532949-dragonrip-toolbar)
+
+## 🛠 Settings
+The <code>settings</code> object has a few boolean settings to customize the clock:
+- <code>runningClock</code>: keep updating the clock even when page is not reloaded (updates once/sec)
+- <code>enableSeconds</code>: show seconds in the time, otherwise only hours and minutes are shown
+- <code>use24HourClock</code>: use 24h clock, 12h is used on default
+- <code>fancyBox</code>: use some additioal CSS styling for the time element
+
+
+
+
+## Preview
+![Dragonrip Server Time 24h preview](https://i.imgur.com/DDSzLCj.png "Dragonrip Server Time 24h preview")
+
+![Dragonrip Server Time 12h preview](https://i.imgur.com/20rScQS.png "Dragonrip Server Time 12h preview")
+
+
+
+
+
+
---
+++ dragonrip-server-time.js
@@ -0,0 +1,153 @@
+// ==UserScript==
+// @name         Dragonrip Server Time
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Server time display for Dragonrip.com
+// @author       Kronos1
+// @match         *://*.dragonrip.com/*
+// @icon         https://i.imgur.com/Vn0ku7D.png
+// @grant        none
+// @license      GPLv3 
+// ==/UserScript==
+
+
+(() => {
+    'use strict';
+
+    const settings = {
+        runningClock: true, // Keep updating clock time (updates every second)
+        enableSeconds: false, // Include seconds in the time
+        use24HourClock: true, // Toggle between 12h/24h time format
+        fancyBox: false, // Additional styling for the time box element
+    }
+    
+    const mainCss = `
+        .dragonrip-server-time-cont > * {
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+            user-select:none;
+        }
+
+        .dragonrip-server-time-cont {
+            border-radius:3px;
+            float:right;
+            display:flex;
+            flex-direction:row;
+            align-items:center;
+            justify-content:space-between;
+            background-repeat:repeat;
+            background-size:contain;
+            padding:3px 5px 3px 5px;
+            font-size:0.8em;
+            font-family:consolas,monospace;
+            color:grey;
+            text-shadow: 
+                0px 0px 3px black,
+                0px 0px 3px black,
+                0px 0px 3px black,
+                0px 0px 3px black,
+                0px 0px 3px black,
+                0px 0px 3px black,
+                0px 0px 3px black
+            ;
+            position:absolute;
+            bottom:0;
+            right:0;
+        }
+
+        .dragonrip-server-time-cont > .label {
+            margin-right:10px;
+        }
+
+        .dragonrip-server-time-cont > .time {
+            color:#de6c09;
+        }
+
+        body > .logo {
+            position:relative;
+        }
+    `;
+
+    const fancyBoxCss = `    
+        .dragonrip-server-time-cont {
+            border:2px solid rgba(255,255,255,0.15);
+            background-image: url('https://i.imgur.com/vjJ8ugC.jpeg');
+            box-shadow:0px 0px 5px 0px rgba(0,0,0,0.5);
+        }
+    `;
+
+    const getTime = () => {
+        let options = {
+            timeZone: 'UTC',
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+        }
+
+        if (settings.enableSeconds) { options.second = 'numeric'; }
+        if (settings.use24HourClock) { options.hour12 = false; }
+        
+        const currentLocalDate  = new Date (Date.now());
+        const time = new Intl.DateTimeFormat([], options).format(currentLocalDate);
+        return time;
+    }
+
+
+    const createClock = () => {
+        const target = document.querySelector('body > div.logo');
+
+        const elem = document.createElement('div');
+        elem.classList.add('dragonrip-server-time-cont');
+   
+        // Create text label
+        const label = document.createElement('div');
+        label.classList.add('label');
+        label.innerHTML = 'Server time';
+
+        // Create time
+        const time = document.createElement('div');
+        time.classList.add('time');
+     
+        elem.append(label);
+        elem.append(time);
+        target.append(elem);
+
+        updateClock();
+ 
+        if (settings.runningClock) {
+            setInterval( () => {
+                updateClock();
+            }, 1000);
+        }
+    }
+
+    const updateClock = () => {
+        const elem = document.querySelector('body > .logo > .dragonrip-server-time-cont > .time');
+        elem.innerText = getTime();
+    }
+
+
+    // Wait for game UI to load, then insert custom element
+    const waitForElem = () => {
+        const checkElem = setInterval( () => {
+            if (document.querySelector('ul.navbar') !== null) {
+                clearInterval(checkElem); 
+                createClock();
+            }
+        }, 5);
+    }
+
+
+    const setCustomCss = str => {
+        const styleElem = document.createElement("style");
+        styleElem.textContent = str;
+        document.body.appendChild(styleElem);
+    }
+
+
+    setCustomCss(mainCss);    
+    if (settings.fancyBox) { setCustomCss(fancyBoxCss); }
+
+    waitForElem();
+})();
